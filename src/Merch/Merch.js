8c6cb2d31f38d772a3f6df19668b90801a10f1bf
TomLeggettDev <tomleggettdev@gmail.com>
2023-05-09 17:55:40 +1200

    added "SHOW MORE" button to Merch section

--- src/Merch/Merch.js
+++ src/Merch/Merch.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import './Merch.css';
 import MerchItem from './MerchItem/MerchItem.js';
 import { currentMerch } from '../variables';
 
+var totalMerchItems = currentMerch.length;
+
+
 function Merch() {
+  const [numItemsToDisplay, setNumItemsToDisplay] = useState(3);
+
+  const changeNumberOfDisplayedItems = () => {
+      setNumItemsToDisplay(Math.min(totalMerchItems, numItemsToDisplay + 3));
+  };
+
   return (
     <section id="merch">
         <h1>MERCH</h1>
         <div id="merchItems">
-          {currentMerch.map((merchItem, index) => <MerchItem item={merchItem} key={index} />)}
+          {currentMerch.map((merchItem, index) => {
+            if( index < numItemsToDisplay ) return <MerchItem item={merchItem} key={index} />
+          })}
         </div>
+        { (numItemsToDisplay < totalMerchItems) && 
+          <button className="Button" onClick={changeNumberOfDisplayedItems}>
+              SHOW MORE
+          </button>
+        }
     </section>
   );
 }
