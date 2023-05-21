8857ff85f5c146417bc44c15ee0fbbd0ac136371
junpark-engagesq <128685930+junpark-engagesq@users.noreply.github.com>
2023-05-22 01:49:42 +1200

    Added Animations

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,20 +1,22 @@
 import './Header.css';
+import React, { useState } from 'react';
 
-	const toggleNavbar = () => {
-  var navBar = document.getElementById('navBar');
-  var menuIcon = document.getElementById("navBarIcon");
 
-  navBar.classList.toggle("hidden");
-  menuIcon.classList.toggle("fa-x");
-  menuIcon.classList.toggle('spin');
-}
 
 function Header() {
+
+  const [isNavBarHidden, setIsNavBarHidden] = useState(true);
+
+  const toggleNavbar = () => {
+    setIsNavBarHidden(!isNavBarHidden);
+    }
+
   return (
     <header id="header">
       <div class='logo'><img src="MissJuneLogoNav.png" alt="Logo"></img></div>
-      <i id="navBarIcon" className="fa-solid fa-lg fa-bars icon" onClick={() => toggleNavbar()}></i>
-      <nav id="navBar" className="hidden">
+      <i id="navBarIcon" className={`fa-solid fa-lg fa-bars icon ${isNavBarHidden ? '' : 'active fa-x'}`}
+        onClick={toggleNavbar}></i>
+      <nav id="navBar" className={isNavBarHidden ? 'hidden' : ''}>
         <span id="mobileSocials">
           <a className="mobileSocialIcon" href="https://www.facebook.com/missjunenz/" target="_blank" rel="norefferer"><i className="fa-brands fa-lg fa-facebook-f icon" onClick={() => toggleNavbar()}></i></a>
           <a className="mobileSocialIcon" href="https://www.instagram.com/ihatemissjune/" target="_blank" rel="norefferer"><i className="fa-brands fa-lg fa-instagram" icon onClick={() => toggleNavbar()}></i></a>
@@ -25,12 +27,12 @@ function Header() {
           <a className="mobileSocialIcon" href="https://soundcloud.com/missjuneband" target="_blank" rel="norefferer" onClick={() => toggleNavbar()}><i className="fa-brands fa-lg fa-soundcloud icon"></i></a>
         </span>
         <ul id="navItems">
-          <li><a className="navItem" href="#" onClick={() => toggleNavbar()}>HOME <div className="underline"></div></a></li>
-          <li><a className="navItem" href="#watch" onClick={() => toggleNavbar()}>WATCH <div className="underline"></div></a></li>
-          <li><a className="navItem" href="#merch" onClick={() => toggleNavbar()}>MERCH <div className="underline"></div></a></li>
-          <li><a className="navItem" href="#shows" onClick={() => toggleNavbar()}>SHOWS <div className="underline"></div></a></li>
-          <li><a className="navItem" href="#about" onClick={() => toggleNavbar()}>ABOUT <div className="underline"></div></a></li>
-          <li><a className="navItem" href="#contact"onClick={() => toggleNavbar()}>CONTACT <div className="underline"></div></a></li>
+          <li><a className={isNavBarHidden ? 'hidden navItem ' : 'navItem slideIn animation-delay-1'} href="#" onClick={() => toggleNavbar()}>HOME <div className="underline"></div></a></li>
+          <li><a className={isNavBarHidden ? 'hidden navItem ' : 'navItem slideIn animation-delay-2'} href="#watch" onClick={() => toggleNavbar()}>WATCH <div className="underline"></div></a></li>
+          <li><a className={isNavBarHidden ? 'hidden navItem ' : 'navItem slideIn animation-delay-3'} href="#merch" onClick={() => toggleNavbar()}>MERCH <div className="underline"></div></a></li>
+          <li><a className={isNavBarHidden ? 'hidden navItem ' : 'navItem slideIn animation-delay-4'} href="#shows" onClick={() => toggleNavbar()}>SHOWS <div className="underline"></div></a></li>
+          <li><a className={isNavBarHidden ? 'hidden navItem ' : 'navItem slideIn animation-delay-5'} href="#about" onClick={() => toggleNavbar()}>ABOUT <div className="underline"></div></a></li>
+          <li><a className={isNavBarHidden ? 'hidden navItem ' : 'navItem slideIn animation-delay-6'} href="#contact"onClick={() => toggleNavbar()}>CONTACT <div className="underline"></div></a></li>
         </ul> 
       </nav>
     </header>
